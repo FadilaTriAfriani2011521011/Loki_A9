1a14ef984b9fd93bbdbcfa7bd44b90824e3801e4
Farrasnaufalsuhanda <farrasnaufalsuhanda14@gmail.com>
2022-09-23 10:15:12 +0700

    layout 4

---
+++ app/src/main/java/id/ac/unand/loki_a9/InputPesertaSeminarKP.java
@@ -0,0 +1,13 @@
+package id.ac.unand.loki_a9;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InputPesertaSeminarKP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_peserta_seminar_kp);
+    }
+}
---
+++ app/src/main/java/id/ac/unand/loki_a9/formLaporanKP.java
@@ -0,0 +1,14 @@
+package id.ac.unand.loki_a9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class formLaporanKP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_laporan_kp);
+    }
+}
