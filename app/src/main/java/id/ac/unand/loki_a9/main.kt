61d81c770882ed267955ec986eebc3eedd6f4fa2
daffamuliya <daffamuliya15@gmail.com>
2022-10-19 23:22:10 +0700

    making a splash screen

--- app/src/main/java/id/ac/unand/loki_a9/main.kt
+++ app/src/main/java/id/ac/unand/loki_a9/main.kt
@@ -1,21 +1,27 @@
 package id.ac.unand.loki_a9
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
+@Suppress("DEPRECATION")
 class main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
+                Handler().postDelayed({
+                    startActivity(Intent(this, login::class.java))
+                    finish()
+                },3000)
 
-        val btn: Button = findViewById(R.id.button3)
-        btn.setOnClickListener {
-            intent = Intent(this, login::class.java)
-            startActivity(intent)
-        }
+            }
+
     }
-}
